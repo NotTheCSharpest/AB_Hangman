b37d69f9d730602fc3c9192fbc15094213e7cff6
NotTheCSharpest <63044477+NotTheCSharpest@users.noreply.github.com>
2020-06-12 08:51:08 +0200

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,19 @@
+/*
+Aufgabe:
+Mach einfach Hangman als Kommandozeilspiele
+
+User-flow
+1. Gruße drucken
+2. Schliefe:
+  A. show obscured word
+  B. take input
+  C. check input against word (hidden)
+  D. update game state
+  E. Loop
+3.
+
+*/
+
+Console.WriteLine("This is a random greeting");
+string hiddenWord = "Donaudampfschiff"
+# 
