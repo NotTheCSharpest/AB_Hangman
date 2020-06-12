db2879113d90b38d4c36fd820dd8d2b746da1319
NotTheCSharpest <63044477+NotTheCSharpest@users.noreply.github.com>
2020-06-12 11:37:10 +0200

    Update gamestate.cs

--- gamestate.cs
+++ gamestate.cs
@@ -5,28 +5,41 @@ namespace Hangman
 {
     public class GameState
     {
+        // create state-holding variables for our object
         private string hiddenWord;
         private list revealedWord;
         private string guesses;
-        public GameState()
+        private int chancesLeft = 6;
+        
+        // not entirely sure why we need this but we do.
+        public GameState(){}
+        
+        // create the target word with user input and instantiate the revealedWord
+        public void createHW()
         {
+            Console.WriteLine("Bitte Ihre Geheimwort eintyppen");
+            hiddenWord = Console.Readline();
+            for (int i = 0; i < hiddenWord.Length; i++)
+            {
+                revealedWord.Add('*');
+            }
         }
-    public void GameState()
-    {
-    }
-    public void createHW()
-    {
-      Console.WriteLine("Bitte Ihre Geheimwort eintyppen");
-      hiddenWord = Console.Readline();
-      for (int i = 0; i < hiddenWord.Length; i++)
-      {
-        revealedWord.Add('*');
-      }
-    }
-    public void guess(char curGuess);
-    {
-    }
-      
+        
+        // our core gameplay, take guess as input and work through possibilities
+        public void guess();
+        {
+            // invalid input
+            // guess has already been made
+            // guess is a new character (gamestate)
+        }
+        
+        public void gameEnd();
+        {
+            //if chancesLeft > 0 -> do winning thing
+            //else ->> do losing thing
+        }
+        
+
       
     // end class  
     }    
