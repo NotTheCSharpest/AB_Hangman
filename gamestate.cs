cb9ebd9f7cb0b162d6816d178b2052a44e6ade5b
Soshi <soshi.mimicry@gmail.com>
2020-06-15 11:35:10 +0200

    tested, working

--- gamestate.cs
+++ gamestate.cs
@@ -1,16 +1,17 @@
 // Our gamestate class
 
 using System;
+using System.Collections.Generic;
 namespace Hangman
 {
     public class GameState
     {
         // create state-holding variables for our object
-        private string eingabe;
-        private string hiddenWord = eingabe.ToLower();
-        private List<char> revealedWord = new List<char>();
-        private string guesses;
-        private int chancesLeft = 6;
+        public string hiddenWord;
+        public List<char> revealedWord = new List<char>();
+        public string guesses = "";
+        public int chancesLeft = 6;
+        public char currGuess;
         
         // not entirely sure why we need this but we do.
         public GameState(){}
@@ -19,8 +20,8 @@ namespace Hangman
         public void createHW()
         {
             Console.WriteLine("Bitte Ihre Geheimwort eintyppen");
-            eingabe = Console.Readline();
-            
+            hiddenWord = Console.ReadLine().ToLower();
+            Console.Clear();
             for (int i = 0; i < hiddenWord.Length; i++)
             {
                 revealedWord.Add('*');
@@ -28,76 +29,172 @@ namespace Hangman
         }
         
         // our core gameplay, take guess as input and work through possibilities
-        public void gameStart();
+        public void gameStart()
         {
-            while ((!chancesLeft = 0) & (revealedWord.Contains(*)))
+            bool isRevealed = revealedWord.Contains('*');
+            bool prevGuesses = guesses.Contains(currGuess);
+            while ((chancesLeft != 0) & isRevealed)
             {
                 // call drawScreen
+                drawScreen();
+                // print revealedWord thus far
+                for (int i = 0; i < revealedWord.Count; i++)
+                {
+                    Console.Write(revealedWord[i]);
+                }
+                Console.WriteLine("");
                 Console.WriteLine("Bitte ihre Vermutung eintyppen");
-                char currGuess = Char.ToLower(Console.ReadLine());
+                char currGuess = Console.ReadKey().KeyChar;
                 // invalid input
-                if (!char.IsLetter(currGuess)
-                    {
+                if (!char.IsLetter(currGuess))
+                {
                     Console.WriteLine("Eingabe Fehler");
                     continue;
                     //Console.WriteLine("Bitte ihre Vermutung eintyppen");
                     //char currGuess = Char.ToLower(Console.ReadLine());
-                    }
+                }
 
                 // guess has already been made
-                else if (guesses.Contains(currGuess))
-                       {
-                       Console.WriteLine("Sie haben das schon probiert!");
-                       continue;
-                       //char currGuess = Char.ToLower(Console.ReadLine());
-                       }
+                else if (prevGuesses)
+                {
+                    Console.WriteLine("Sie haben das schon probiert!");
+                    continue;
+                    //char currGuess = Char.ToLower(Console.ReadLine());
+                }
 
                 // guess is a valid new character (gamestate)
                 else if (hiddenWord.Contains(currGuess))
-                       {
-                       // update revealedWord from asterix to letter
-                           for (int i = 0; while i < hiddenWord.Length; i++)
-                           {
-                               if (hiddenWord[i] = currGuess)
-                               {
-                               revealedword[i] = currGuess
-                               }
-                           }
-                           //call game-end condition
-                            if (!revealedWord.Contains(*)
-                                {
+                {
+                    // update revealedWord from asterix to letter
+                    for (int i = 0; i <= hiddenWord.Length - 1; i++)
+                    {
+                        if (currGuess == hiddenWord[i])
+                        {
+                            revealedWord[i] = currGuess;
+                            if (revealedWord.Contains('*') == false)
+                            {
                                 Console.Clear();
-                                Console.WriteLine("Sie haben gewonnen!");
-                                }
-                                
-                            elseif (chancesLeft = 0)
-                                {
-                                Console.Clear();
-                                Console.WriteLine("Sie haben verlieren");
-                                }
-                            else
-                                {
-                                    continue;
-                                }
-                           
-                       }
+                                Console.WriteLine("Sie haben gewonnen");
+                                return;
+                            }
+                        }
+
+                    }
+                    /*
+                    //call game-end condition
+                 if (isRevealed == false)
+                         {
+                         Console.Clear();
+
+                         Console.WriteLine("Sie haben gewonnen!");
+                         }*/
+                }
+
+                
+                
                 else if (!hiddenWord.Contains(currGuess))
                        {
                        // update hangman, chancesLeft = -1
                            chancesLeft = chancesLeft-1;
-                           continue;
+                    if (chancesLeft <= 0)
+                        {
+                            Console.Clear();
+
+                            Console.WriteLine("Sie haben verlieren");
+                        }
+                    continue;
                        }
-            //ends while loop
+                else
+                {
+                    continue;
+                }
+
+                //ends while loop
             }
 
-        // game end loop
-           
+            // game end loop
+
         }
     
-        public void drawScreen():
+        public void drawScreen()
         {
             Console.Clear();
-            
+            Console.WriteLine("_____________");
+            Console.WriteLine("|           |");
+            if (chancesLeft == 6)
+            {
+                Console.WriteLine("|               ");
+                Console.WriteLine("|               ");
+                Console.WriteLine("|               ");
+                Console.WriteLine("|               ");
+            }
+            else if (chancesLeft == 5)
+            {
+
+                Console.WriteLine("|           |   ");
+                Console.WriteLine("|           o   ");
+                Console.WriteLine("|          o o  ");
+                Console.WriteLine("|           o   ");
+
+            }
+            else if (chancesLeft == 4)
+            {
+                Console.WriteLine("|           |   ");
+                Console.WriteLine("|           o   ");
+                Console.WriteLine("|          o o  ");
+                Console.WriteLine("|           o   ");
+                Console.WriteLine("|           |   ");
+                Console.WriteLine("|           |   ");
+                Console.WriteLine("|           |   ");
+                Console.WriteLine("|           |   ");
+            }
+            else if (chancesLeft == 3)
+            {
+                Console.WriteLine("|           |   ");
+                Console.WriteLine("|           o   ");
+                Console.WriteLine("|          o o  ");
+                Console.WriteLine("|           o   ");
+                Console.WriteLine("|           |   ");
+                Console.WriteLine("|          /|  ");
+                Console.WriteLine("|           |   ");
+                Console.WriteLine("|           |   ");
+            }
+            else if (chancesLeft == 2)
+            {
+                Console.WriteLine("|           |   ");
+                Console.WriteLine("|           o   ");
+                Console.WriteLine("|          o o  ");
+                Console.WriteLine("|           o   ");
+                Console.WriteLine("|           |   ");
+                Console.WriteLine("|          /|\\  ");
+                Console.WriteLine("|           |   ");
+                Console.WriteLine("|           |   ");
+            }
+            else if (chancesLeft == 1)
+            {
+                Console.WriteLine("|           |   ");
+                Console.WriteLine("|           o   ");
+                Console.WriteLine("|          o o  ");
+                Console.WriteLine("|           o   ");
+                Console.WriteLine("|           |   ");
+                Console.WriteLine("|          /|\\  ");
+                Console.WriteLine("|           |   ");
+                Console.WriteLine("|          /|   ");
+            }
+            else if (chancesLeft == 0)
+            {
+                Console.WriteLine("|           |   ");
+                Console.WriteLine("|           o   ");
+                Console.WriteLine("|          o o  ");
+                Console.WriteLine("|           o   ");
+                Console.WriteLine("|           |   ");
+                Console.WriteLine("|          /|\\  ");
+                Console.WriteLine("|           |   ");
+                Console.WriteLine("|          /|\\  ");
+            }
+            else
+                Console.WriteLine("Fehler, zu viele Chancen");
+                return;
             /*
             1. Clear Screen
             2. Redraw Screen /w revealedWord (with formatting), prompt, and Hangman
@@ -105,7 +202,7 @@ namespace Hangman
             https://docs.microsoft.com/en-us/dotnet/api/system.console.clear?view=netcore-3.1
             https://docs.microsoft.com/en-us/dotnet/api/system.drawing.graphics.clear?view=dotnet-plat-ext-3.1
          */
-        //ends drawScreen function
+            //ends drawScreen function
         }
 
       
--- program.cs
+++ program.cs
@@ -1,71 +1,18 @@
-using System;
+﻿using System;
+
 namespace Hangman
-
 {
-
-
     class Program
     {
-    // print greeting
-    Console.WriteLine("Herzlich Wilkommen bei Hangman");
-    // start game (create hidden word, etc.)
-    GameState.createHW();
-    // Start the actual game loop
-    GameState.gameStart();
+        static void Main(string[] args)
+        {
+            // print greeting
+            Console.WriteLine("Herzlich Wilkommen bei Hangman");
+            // start game (create hidden word, etc.)
+            GameState newGame = new GameState();
+            newGame.createHW();
+            // Start the actual game loop
+            newGame.gameStart();
+        }
     }
-   
-    
 }
-
-/* all of this is now outdated, left for reference only
-// declaring our data objects to hold state during the game  
-string hiddenWord = "Donaudampfschiff"
-List<char> revealedWord = new List<char>();
-List<char> guesses = new List<char>();
-
-// functions
-
-// create function to set up ano(check syntax)
-def createRevealed():
-    int i = hiddenWord.Length();
-    int c = 0;
-    while c < i
-        revealedWord.Add(*);
-
-
-Console.WriteLine("This is a random greeting");
-
-/*
-List<char> revealedWord = new List<char>();
-revealedWord.Add('*');
-revealedWord.Add('*');
-revealedWord.Add('*');
-revealedWord[1] = '!';
-int i = 0;
-while (i < revealedWord.Count)
-{
-    Console.WriteLine(revealedWord[i]);
-    i++;
-    }
-*
-!
-*
-
-Function to build our variables
-from https://www.learncs.org/en/Lists#:~:text=Lists%20in%20C%23%20are%20very,defined%20using%20the%20generic%20syntax.
-List<int> numbers = new List<int>();
-
-Pseudocode:
-#create list
-List<char> revealedWord = new List<char>();
-numbers.Add(1);
-numbers.Add(2);
-#revealedWord needs to start out with a number of * = to length of word
-
-#create function (check syntax)
-def createRevealed():
-    int i = hiddenWord.Length();
-    int c = 0;
-    while c < i
-        revealedWord.Add(*);
-*/
